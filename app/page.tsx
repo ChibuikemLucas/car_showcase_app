bff64cfa7282ae5266bbbde9f159e2d2bb069c57
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-01-19 20:53:05 +0100

    Create hero component, set up components folder and add index.ts file for importing all components in thir necessary pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { Hero } from "@/components";
 
 export default function Home() {
   return (
-       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       <main className="overflow-hidden">
+        <Hero />
       </main>
   );
 }
