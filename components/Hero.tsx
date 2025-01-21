f4e359db5d04467e3e404760b6102ed8fd0509af
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-01-21 21:11:02 +0100

    Complete opening header and paragraph for hero page and start custom button.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
   return (
     <div className="Hero">
       <div className="flex-1 pt-6 padding-x">
         <h1 className="hero__title"> 
-          Find, book, or rent a car - quickly and easily!
+          Find, book, or rent a car — quickly and easily!
         </h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with
+          our effortless booking process.
+        </p>
+
+        <CustomButton 
+        
+        
+        />
       </div>
     </div>
   )
