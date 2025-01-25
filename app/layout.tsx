6d26ee16915448a1a09c3a56b51152ded0796f9e
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-01-25 23:15:22 +0100

    Continue editing layout and create Navbar and Footer components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+//import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Footer, Navbar } from "@/components";
 
-const geistSans = Geist({
+{/*const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+});*/}
 
-const geistMono = Geist_Mono({
+{/*const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+});*/}
 
 export const metadata: Metadata = {
   title: "Car Hub",
@@ -25,9 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="relative"
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
