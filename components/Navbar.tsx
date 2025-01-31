40cabe6e801c4f785fd72c4640a52bd82f115976
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-01-31 21:30:42 +0100

    Add link tag in the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,9 @@ const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
       <nav className="max-w-[144px] flex justify-between items-center sm:px-16 px-6 py-4s">
-
+      <Link href="/" className="flex justify-center
+       items-center">
+      </Link>
       </nav>
     </header>
   )
