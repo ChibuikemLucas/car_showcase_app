95c16923190a904dc4260e73c3278c44fb77e198
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-01-31 21:33:53 +0100

    Add logo image inside link tag

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,6 +9,12 @@ const Navbar = () => {
       <nav className="max-w-[144px] flex justify-between items-center sm:px-16 px-6 py-4s">
       <Link href="/" className="flex justify-center
        items-center">
+        <Image 
+         src="/logo.svg"
+         alt="Car Hub Logo"
+         width={118}
+         height={18}
+         className="object-contain"/>
       </Link>
       </nav>
     </header>
