a4b6745ba5d3fd18957681e72cddbaae57343d4d
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-01-31 21:27:59 +0100

    Create nav and give it some classnames.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import CustomButton from "./CustomButton"
 const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
-      <nav>
+      <nav className="max-w-[144px] flex justify-between items-center sm:px-16 px-6 py-4s">
 
       </nav>
     </header>
