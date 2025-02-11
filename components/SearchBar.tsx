56252254751a5ae4d7c88fdeaf262a017b044fcf
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-02-11 16:40:41 +0100

    Create SearchManufacturer component.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+
+import SearchManufacturer from "./SearchManufacturer";
 
 const SearchBar = () => {
     const handleSearch = () => {}
   return (
     <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
     </form>
   )
 }
