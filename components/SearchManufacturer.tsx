d8cee255fdd316d6075a8783b922bb901db820e8
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-03-27 18:30:48 +0100

    Create combobox input.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import { ComboboxButton } from '@headlessui/react';
+import { useState } from 'react';
+import { ComboboxButton, ComboboxInput } from '@headlessui/react';
 import { Combobox, Transition } from '@headlessui/react'
 import Image from 'next/image';
 import { SearchManufacturerProps } from '@/types'
 
 const SearchManufacturer = ({manufacturer, setManufacturer} 
     :SearchManufacturerProps) => {
+
+const [query, setQuery] = useState('')  
+      
   return (
     <div className='search-manufacturer'>
         <Combobox>
             <div className="relative w-full">
              <ComboboxButton className="absolute top-[14px]">
-              
+
               <Image 
               src="/car-logo.svg"
               width={20}
@@ -22,6 +26,13 @@ const SearchManufacturer = ({manufacturer, setManufacturer}
               />
 
              </ComboboxButton>
+
+             <ComboboxInput 
+             className="search-manufacturer_input"
+             placeholder="VolksWagen"
+             displayValue={(manufacturer: string) => manufacturer}
+             onChange={(e)=> setQuery(e.target.value)}
+             />
             </div>
         </Combobox>
     </div>
