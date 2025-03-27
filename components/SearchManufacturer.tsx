6c3a37db4142a329b424f810ef7b746b39d663c0
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-03-27 19:25:45 +0100

    Add filter for combo box options.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,15 +1,27 @@
 "use client";
 
 import { useState, Fragment } from 'react';
-import { ComboboxButton, ComboboxInput } from '@headlessui/react';
+import { ComboboxButton, ComboboxInput, ComboboxOptions } from '@headlessui/react';
 import { Combobox, Transition } from '@headlessui/react'
+import { manufacturers } from '@/constants';
 import Image from 'next/image';
 import { SearchManufacturerProps } from '@/types'
 
 const SearchManufacturer = ({manufacturer, setManufacturer} 
     :SearchManufacturerProps) => {
 
+
 const [query, setQuery] = useState('')  
+
+const filteredManufacturers = 
+  query === "" 
+  ? manufacturers  
+  : manufacturers.filter((item)=> (
+     item.toLowerCase()
+     .replace(/\s+/g, "")
+     .includes(query.toLowerCase().replace(/\s+/g, ""
+    )))
+)
       
   return (
     <div className='search-manufacturer'>
@@ -41,7 +53,9 @@ const [query, setQuery] = useState('')
              leaveTo="opacity-0"
              afterLeave={() => setQuery('')}
              >
-
+             <ComboboxOptions>
+              
+              </ComboboxOptions> 
 
              </Transition>
             </div>
