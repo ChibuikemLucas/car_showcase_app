574abd4635f91f243f5f0b8c58656d0e25930aaa
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-04-23 12:38:24 +0100

    Create utility function.

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,14 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'ef38d5dcdbmsh69425693823ab08p17c326jsnc9411c6025c3',
+export async function fetchCars (){
+    const headers = {	
+        'x-rapidapi-key': 'ef38d5dcdbmsh69425693823ab08p17c326jsnc9411c6025c3',
 		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+    }
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',{
+        headers: headers,
+    });
+
+    const result = await response.json();
+
+    return result;
 }
