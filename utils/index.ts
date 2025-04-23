11bce604c75e5d6c1ce86406d9c84cdf04c06f64
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-04-23 12:27:05 +0100

    Make API request.

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'ef38d5dcdbmsh69425693823ab08p17c326jsnc9411c6025c3',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
