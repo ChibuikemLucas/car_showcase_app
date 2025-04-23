68390862f75ffdf0005d59ef7de27378b4f845f4
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-04-23 13:04:38 +0100

    Add API to pages.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-//import Image from "next/image";
-import { Hero } from "@/components";
-import SearchBar from "@/components/SearchBar";
-import CustomFilter from "@/components/CustomFilter";
+import Image from "next/image";
+
+import { CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 //import { Calendar } from "@/components";
 
-export default function Home() {
+export default async function Home() {
+   const allCars = await fetchCars();
+
+   const isDataEmpty = !Array.isArray(allCars) ||
+    allCars.length < 1 || !allCars;
+
   return (
        <main className="overflow-hidden">
         <Hero />
@@ -21,14 +26,30 @@ export default function Home() {
             <SearchBar />
 
             <div className="home__filter-container">
-               <CustomFilter />
-               <CustomFilter />
+               <CustomFilter title="fuel"/>
+               <CustomFilter title="year"/>
             </div>
 
             <div>
              
             </div>
           </div>
+
+          {!isDataEmpty} ? (
+            <section>
+              <div className="home__cars-wrapper">
+               {allCars?.map((car) => (
+               <CarCard car={car} />
+               ))}
+              </div>
+            </section>
+          ):(
+            <div className="home__error-container">
+              <h2 className="text-black text-xl 
+               font-bold">Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
         </div>
       </main>
   );
