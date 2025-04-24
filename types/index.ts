e08e8128180b681c4d73d8802528bb827fd5f6ce
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-04-24 11:32:26 +0100

    Create car prop.

--- types/index.ts
+++ types/index.ts
@@ -12,3 +12,18 @@ export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer: string) => void;
 }
+
+export interface CarProps {
+        "city_mpg": "this field is for premium subscribers only",
+          "class": "compact car",
+          "combination_mpg": "this field is for premium subscribers only",
+          "cylinders": 4,
+          "displacement": 1.6,
+          "drive": "fwd",
+          "fuel_type": "gas",
+          "highway_mpg": "this field is for premium subscribers only",
+          "make": "toyota",
+          "model": "corolla",
+          "transmission": "a",
+          "year": 1993
+  }
