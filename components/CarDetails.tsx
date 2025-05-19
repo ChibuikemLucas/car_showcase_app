718698205686b5618f5a3f7d76d2a0a818a40c36
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-05-19 14:57:40 +0100

    feat: Start structuring car details component.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from 'next/image';
 import { Fragment } from 'react';
 import { CarProps } from '@/types';
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog, Transition, TransitionChild } from '@headlessui/react';
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -11,7 +13,23 @@ interface CarDetailsProps {
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
-    <div>CarDetails</div>
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+          <Dialog as='div' className='relative z-10' onClose={closeModal}>
+            <TransitionChild
+             as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+            >
+              <div className='fixed inset-0 bg-black bg-opacity-25' />
+            </TransitionChild>
+          </Dialog>
+      </Transition>
+    </>
   )
 }
 
