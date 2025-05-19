7188e212d23d35c5e462fe2c795f94a16102babf
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-05-19 15:21:20 +0100

    feat: Add divs for different car views.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -59,8 +59,28 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <Image src="/hero.png" 
                           alt="car-model" fill priority className="object-contain"
                           />
-                    </div>  
+                    </div>
+
+                    <div className='flex gap-3'> 
+                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image src="/hero.png" 
+                          alt="car-model" fill priority className="object-contain"
+                          />
+                    </div> 
+                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image src="/hero.png" 
+                          alt="car-model" fill priority className="object-contain"
+                          />
+                    </div> 
+                    <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                        <Image src="/hero.png" 
+                          alt="car-model" fill priority className="object-contain"
+                          />
+                    </div> 
+                  </div>  
                 </div>
+
+                
              </DialogPanel>
             </TransitionChild>
                  </div>
