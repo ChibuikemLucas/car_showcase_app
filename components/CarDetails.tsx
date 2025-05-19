ff35517e15ef9e20148b971853bd06fc7fa06b1c
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-05-19 15:07:49 +0100

    feat: Add button and image to dialog module.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -40,7 +40,19 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
           leaveTo='opacity-0  scale-95'
             >
              <DialogPanel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5'>
-
+               <button
+                  type='button'
+                  className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
+                  onClick={closeModal}
+                >
+                  <Image
+                    src='/close.svg'
+                    alt='close'
+                    width={20}
+                    height={20}
+                    className='object-contain'
+                  />  
+                </button>
              </DialogPanel>
             </TransitionChild>
                  </div>
