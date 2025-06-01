01a532db547c4a2d2c747e1f9f429bc50a988034
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-06-01 13:55:58 +0100

    feat: Continue editing custom filter.

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,13 +3,28 @@
 import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, ListboxButton, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
 
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]); // State for storing the selected option
   return (
-    <div>CustomFilter</div>
+    <div className='w-fit'>
+      <Listbox  
+      value={selected}
+        onChange={(e) => {
+          setSelected(e); // Update the selected option in state
+         // handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
+        }}>
+    <div className='relative w-fit z-10'>
+      <ListboxButton className='custom-filter__btn'>
+       <span className='block truncate'>{selected.title}</span>
+      
+      </ListboxButton>
+    </div>
+    </Listbox>
+    </div>
   )
 }
 
