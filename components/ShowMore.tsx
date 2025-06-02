213fdaa5cd6f190212e74ae86104a46656281cbb
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-06-02 14:14:38 +0100

    feat: Create a function to calculate the new limit based on the page number and navigation type.

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -2,10 +2,29 @@
 
 import { useRouter } from "next/navigation";
 import { ShowMoreProps } from "@/types";
+import CustomButton from "./CustomButton";
 
 const ShowMore = ({ pageNumber, isNext } : ShowMoreProps) => {
+  
+  const router = useRouter();
+  
+  const handleNavigation = () => {
+      const newLimit = (pageNumber + 1) * 10;
+     const newPathname = updateSearchParams("limit", `${newLimit}`);
+  }
+    
+
   return (
-    <div>ShowMore</div>
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
   )
 }
 
